86c3b10a30c3ac5ffd3cb65e5063a19ccebb9da1
DakMar <b031810044@student.utem.edu.my>
2020-08-24 14:56:44 +0800

    first commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,7 @@
+<!-- ============ Inject Your Layout ============= -->
+
+
+@include('layouts.vertical-sidebar.master')
+
+
+<!-- ============ Or you can directly Extends any master layouts  ============= -->
